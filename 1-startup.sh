05565b7a90bed5f1f85fe58f193d93d1ababdbda
Hoang Tran <trbhoang@gmail.com>
2019-12-15 21:55:52 +0700

    disable LFD excessive resource usage alert

--- 1-startup.sh
+++ 1-startup.sh
@@ -156,6 +156,11 @@ sed -i 's/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP_OUT = "22,80
 sed -i 's/UDP_IN = "20,21,53"/UDP_IN = ""/g' /etc/csf/csf.conf
 sed -i 's/UDP_OUT = "20,21,53,113,123"/UDP_OUT = ""/g' /etc/csf/csf.conf
 
+# disable LFD excessive resource usage alert
+# ref: https://www.interserver.net/tips/kb/disable-lfd-excessive-resource-usage-alert/
+sed -i 's/PT_USERMEM = /PT_USERMEM = 0 #/g' /etc/csf/csf.conf
+sed -i 's/PT_USERTIME = /PT_USERTIME = 0 #/g' /etc/csf/csf.conf
+
 # Ignore alert if following process use exeeded resource
 echo "exe:/usr/sbin/rsyslogd" | tee --append /etc/csf/csf.pignore
 echo "exe:/lib/systemd/systemd-networkd" | tee --append /etc/csf/csf.pignore
